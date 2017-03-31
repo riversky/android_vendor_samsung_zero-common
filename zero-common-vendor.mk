26aff0a42a095aff072f93f9a47960180e492cf2
Lukas0610 <mail@lukasberger.at>
2017-03-31 22:32:09 +0200

    zero: readded fingerprint-blob

--- zero-common-vendor.mk
+++ zero-common-vendor.mk
@@ -129,7 +129,8 @@ PRODUCT_COPY_FILES += \
     $(BLOB_PATH)/lib64/libbauthserver.so:system/lib64/libbauthserver.so \
     $(BLOB_PATH)/lib64/libbauthtzcommon.so:system/lib64/libbauthtzcommon.so \
     $(BLOB_PATH)/lib64/libsynaFpSensorTestNwd.so:system/lib64/libsynaFpSensorTestNwd.so \
-    $(BLOB_PATH)/lib64/libegis_fp_normal_sensor_test.so:system/lib64/libegis_fp_normal_sensor_test.so
+    $(BLOB_PATH)/lib64/libegis_fp_normal_sensor_test.so:system/lib64/libegis_fp_normal_sensor_test.so \
+    $(BLOB_PATH)/lib64/hw/fingerprint.default.so:system/lib64/hw/fingerprint.default.so
 
 # Graphics
 PRODUCT_COPY_FILES += \
