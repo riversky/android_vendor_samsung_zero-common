69b5f00ca99351c6ef895d27dca548ff583c8def
Luca Polesel <lucapolesel@outlook.it>
2017-04-15 11:48:03 +0200

    zero: Add libcamera_metadata

--- zero-common-vendor.mk
+++ zero-common-vendor.mk
@@ -232,6 +232,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(BLOB_PATH)/lib/hw/camera.exynos5.so:system/lib/hw/camera.exynos5.so \
     $(BLOB_PATH)/lib64/hw/camera.exynos5.so:system/lib64/hw/camera.exynos5.so \
+    $(BLOB_PATH)/lib/libcamera_metadata.so:system/lib/libcamera_metadata.so \
+    $(BLOB_PATH)/lib64/libcamera_metadata.so:system/lib64/libcamera_metadata.so \
     $(BLOB_PATH)/lib/libexynoscamera3.so:system/lib/libexynoscamera3.so \
     $(BLOB_PATH)/lib64/libexynoscamera3.so:system/lib64/libexynoscamera3.so \
     $(BLOB_PATH)/lib/libexynoscamera.so:system/lib/libexynoscamera.so \
