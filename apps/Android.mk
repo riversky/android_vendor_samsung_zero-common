2426aa8a945188a3f1a2e25ff9868a903494fbe1
Lukas0610 <mail@lukasberger.at>
2017-10-30 12:13:49 +0100

    zero: fixed definition of HealthService
    Change-Id: I04c79232a37ab7a460660a4340aaad239fdd4c2a

--- apps/Android.mk
+++ apps/Android.mk
@@ -1,12 +1,11 @@
 #
-# Copyright (C) 2016 The CyanogenMod Project
 # Copyright (C) 2017 TeamNexus
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,5 +15,4 @@
 #
 
 LOCAL_PATH := $(call my-dir)
-
-include $(LOCAL_PATH)/apps/Android.mk
+include $(call all-subdir-makefiles,$(LOCAL_PATH))
--- apps/HealthService/Android.mk
+++ apps/HealthService/Android.mk
@@ -1,12 +1,11 @@
 #
-# Copyright (C) 2016 The CyanogenMod Project
 # Copyright (C) 2017 TeamNexus
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -17,4 +16,14 @@
 
 LOCAL_PATH := $(call my-dir)
 
-include $(LOCAL_PATH)/apps/Android.mk
+include $(CLEAR_VARS)
+LOCAL_MODULE               := HealthService
+LOCAL_SRC_FILES            := $(LOCAL_MODULE).apk
+LOCAL_MODULE_TAGS          := optional
+LOCAL_MODULE_CLASS         := APPS
+LOCAL_MODULE_SUFFIX        := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_PRIVILEGED_MODULE    := false
+LOCAL_BUILT_MODULE_STEM    := package.apk
+LOCAL_CERTIFICATE          := PRESIGNED
+LOCAL_DEX_PREOPT           := false
+include $(BUILD_PREBUILT)
